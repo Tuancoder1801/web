455deeea85a0635bcf73d81e9478379e5a962d68
Tuancoder1801 <tuantd1801@gmail.com>
2025-01-11 15:34:38 +0700

    init

---
+++ web/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
+
+///Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('admin')->group(function () {
+        // Route::get('/', [MainController::class, 'index'])->name('admin');
+        // Route::get('main', [MainController::class, 'index']);
+
+        #Category
+        Route::prefix('categories')->group(function () {
+            Route::get('add', [CategoryController::class, 'create']);
+            Route::post('add', [CategoryController::class, 'store']);
+        });
+
+        Route::prefix('products')->group(function () {
+            Route::get('add', [ProductController::class, 'create']);
+            Route::post('add', [ProductController::class, 'store']);
+        });
+    });
+///});
+
