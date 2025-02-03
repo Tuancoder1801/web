fd6f1b9eef84c8bb492357bda8f667cf59cd46f1
TuanTD <135809648+Tuancoder1801@users.noreply.github.com>
2025-02-04 00:07:07 +0700

    update

--- web/resources/views/admin/sidebar.blade.php
+++ web/resources/views/admin/sidebar.blade.php
@@ -2,8 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/admin" class="brand-link">
-        <img src="/template/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
+        <img src="/template/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -12,7 +12,8 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="https://storage.googleapis.com/blogvxr-uploads/2020/10/A%CC%89nh-de%CC%A3p-Vie%CC%A3%CC%82t-Nam.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="https://storage.googleapis.com/blogvxr-uploads/2020/10/A%CC%89nh-de%CC%A3p-Vie%CC%A3%CC%82t-Nam.jpg"
+                    class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">Admin</a>
@@ -22,8 +23,7 @@
         <!-- SidebarSearch Form -->
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                    aria-label="Search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
                     <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
@@ -34,9 +34,8 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
-                
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-bars"></i>
@@ -81,7 +80,29 @@
                                 <p>Danh Sách Sản Phẩm</p>
                             </a>
                         </li>
+                    </ul>
+                </li>
 
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa-regular fa-image"></i>
+                        <p> Slider
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="/admin/sliders/add" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Thêm Slider</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/admin/sliders/list" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Danh Sách Slider</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
             </ul>
--- web/routes/web.php
+++ web/routes/web.php
@@ -4,36 +4,45 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\UploadController;
-use App\Models\Category;
-use App\Models\Product;
+use App\Http\Controllers\Admin\SliderController;
 
 ///Route::middleware(['auth'])->group(function () {
 
-    Route::prefix('admin')->group(function () {
-        // Route::get('/', [MainController::class, 'index'])->name('admin');
-        // Route::get('main', [MainController::class, 'index']);
+Route::prefix('admin')->group(function () {
+    // Route::get('/', [MainController::class, 'index'])->name('admin');
+    // Route::get('main', [MainController::class, 'index']);
 
-        #Category
-        Route::prefix('categories')->group(function () {
-            Route::get('add', [CategoryController::class, 'create']);
-            Route::post('add', [CategoryController::class, 'store']);
-            Route::get('list', [CategoryController::class, 'index']);
-            Route::get('edit/{category}', [CategoryController::class, 'show']);
-            Route::post('edit/{category}', [CategoryController::class, 'update']);
-            Route::DELETE('destroy', [CategoryController::class, 'destroy']);
-        });
-
-        Route::prefix('products')->group(function () {
-            Route::get('add', [ProductController::class, 'create']);
-            Route::post('add', [ProductController::class, 'store']);
-            Route::get('list', [ProductController::class, 'index']);
-            Route::get('edit/{product}', [ProductController::class, 'show']);
-            Route::post('edit/{product}', [ProductController::class, 'update']);
-            Route::DELETE('destroy', [ProductController::class, 'destroy']);
-        });
-
-        #Upload
-        Route::post('upload/services', [UploadController::class, 'store']);
+    #Category
+    Route::prefix('categories')->group(function () {
+        Route::get('add', [CategoryController::class, 'create']);
+        Route::post('add', [CategoryController::class, 'store']);
+        Route::get('list', [CategoryController::class, 'index']);
+        Route::get('edit/{category}', [CategoryController::class, 'show']);
+        Route::post('edit/{category}', [CategoryController::class, 'update']);
+        Route::DELETE('destroy', [CategoryController::class, 'destroy']);
     });
-///});
 
+    #Category
+    Route::prefix('products')->group(function () {
+        Route::get('add', [ProductController::class, 'create']);
+        Route::post('add', [ProductController::class, 'store']);
+        Route::get('list', [ProductController::class, 'index']);
+        Route::get('edit/{product}', [ProductController::class, 'show']);
+        Route::post('edit/{product}', [ProductController::class, 'update']);
+        Route::DELETE('destroy', [ProductController::class, 'destroy']);
+    });
+
+    #slider
+    Route::prefix('sliders')->group(function () {
+        Route::get('add', [SliderController::class, 'create']);
+        // Route::post('add', [SliderController::class, 'store']);
+        // Route::get('list', [SliderController::class, 'index']);
+        // Route::get('edit/{product}', [SliderController::class, 'show']);
+        // Route::post('edit/{product}', [SliderController::class, 'update']);
+        // Route::DELETE('destroy', [SliderController::class, 'destroy']);
+    });
+
+    #Upload
+    Route::post('upload/services', [UploadController::class, 'store']);
+});
+///});
